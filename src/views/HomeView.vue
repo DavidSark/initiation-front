7f5643ee5648f5b4cfe77f34eb8fde0a0e59786f
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-10-02 15:44:21 +0200

    fix(MyButton) : renommage du fichier MyButton

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
-import MyButton from '@/components/elements/index.vue'
-import MyButtonVue3 from '@/components/elements/indexVue3.vue'
+import MyButton from '@/components/elements/MyButton.vue'
+// import MyButtonVue3 from '@/components/elements/MyButton2.vue'
 import Layout from '@/components/layouts/DefaultLayout.vue'
 import MyBackgroundScroll from '@/components/MyBackgroundScroll.vue'
 import MyIcon from '@/components/elements/MyIcon.vue'
