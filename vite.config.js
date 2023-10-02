e5639cda58a038bcf93a3afcab0bccdf18a2cd4f
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-10-02 09:19:35 +0200

    feat(package.json) : modification du fichier JSON

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@/scss/foundations/_variables.scss";
+        @import "@/scss/foundations/_mixins.scss";`
+      }
+    }
   }
 })
