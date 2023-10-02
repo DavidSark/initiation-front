18311af03dfbdbfc70ad0434b89ab83564a9c8e1
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-10-02 14:51:26 +0200

    feat(background-scroll) : background scroll composition

--- src/components/MyBackgroundScroll.vue
+++ src/components/MyBackgroundScroll.vue
@@ -1,32 +1,32 @@
-<script>
-export default{
-    data(){
-        return{
-            percent: 0
-        }
-    },
-    methods:{
-        handleScroll(){
-            let scrollTop = window.scrollY
-            let scrollBottom = scrollTop + window.innerHeight
-           
+<script setup>
 
-            let offsetTop = this.$refs.container.offsetTop
-            let elementHeight = this.$refs.container.offsetHeight
-           
-            let percent = (((scrollBottom - offsetTop)/ elementHeight) * 100);
+import {ref, onMounted, onUnmounted }from 'vue'
 
-            this.percent = Math.round(percent)
 
-        }
-    },
-    mounted(){
-        window.addEventListener('scroll', this.handleScroll)
-    },
-    unmounted(){
-        window.removeEventListener('scroll', this.handleScroll)
-    }
+const percent = ref(0)
+const container = ref()
+
+
+
+function handleScroll(){
+    let scrollTop = window.scrollY
+    let scrollBottom = scrollTop + window.innerHeight
+
+    let offsetTop = container.value.offsetTop
+    let elementHeight = container.value.offsetHeight
+          
+    let result = ((scrollBottom - offsetTop)/ elementHeight) * 100;
+
+    percent.value = result  > 100 ? 100 : Math.round(result)
 }
+
+
+    onMounted(() =>{
+        window.addEventListener('scroll', handleScroll)
+    })
+    onUnmounted(()=>{
+        window.removeEventListener('scroll', handleScroll)
+    })
 
 </script>
 
