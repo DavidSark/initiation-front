2fa213188e95de55bc2a574ec6c7374d97a50914
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-10-02 09:19:01 +0200

    feat(client) : création de bouton

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,51 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import MyButton from '@/components/elements/index.vue'
+import MyButtonVue3 from '@/components/elements/indexVue3.vue'
+import Layout from '@/components/layouts/DefaultLayout.vue'
+import MyBackgroundScroll from '@/components/MyBackgroundScroll.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Layout>
+      <template v-slot:header>
+        <nav>
+          <ul>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">link</a></li>
+          </ul>
+        </nav>
+      </template>
+     
+        <template v-slot:aside>
+        <nav>
+          <ul>
+            <li><a href="#">Aside link</a></li>
+            <li><a href="#">Aside link</a></li>
+            <li><a href="#">Aside link</a></li>
+          </ul>
+        </nav>
+        
+      </template>
+      
+      <MyBackgroundScroll />
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aperiam neque quam perferendis, quo blanditiis
+        ratione, soluta eum nobis quaerat aut modi eligendi voluptatum, in numquam tempore optio. Temporibus, minus.</p>
+      <template v-slot:footer>
+        <MyButton>Bouton</MyButton>
+      </template>
+
+    </Layout>
+
+
+
+    <!-- <MyButton size="small" variant="rounded" href="/about" >My button</MyButton>
+   <MyButton href="/about" >My button</MyButton> -->
+    <!-- <MyButton variant="rounded" >My big rounded button</MyButton> -->
+
+    <!-- <MyButtonVue3 >My big rounded button</MyButtonVue3> -->
+    <!-- <MyButton>My button 2</MyButton> -->
   </main>
 </template>
