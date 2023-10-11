f9d7e380fbb13e5a69d5cf8d30c36ab72454460e
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-10-11 10:29:29 +0200

    feat(card location) : début des cards location

---
+++ src/components/LocationCards.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MyIconVue from './elements/MyIcon.vue';
+
+MyIconVue
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
